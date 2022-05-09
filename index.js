d9848baa30f2a4a0b5153e677f36535e5a5bf4f3
wchristian28 <69280519+wchristian28@users.noreply.github.com>
2022-05-09 15:05:52 -0500

    src

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './containers/App';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+reportWebVitals();
